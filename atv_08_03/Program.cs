ed087b8da2c790d35d73126d5d27920a4db2c672
Mateus Barreto <127315407+War02@users.noreply.github.com>
2023-03-15 10:02:00 -0300

    Update Program.cs

--- atv_08_03/Program.cs
+++ atv_08_03/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 namespace atv_08_03
 {
     class Program
@@ -10,13 +10,33 @@ namespace atv_08_03
             Endereco e1 = new Endereco(1, "15990630", "Av. 15 de Novembro", 114, "Nova Matão", "Matão-SP");
 
             Console.WriteLine("\t---------- Cadastro ----------");
-            Console.WriteLine("ID: " + c1.getId() + "\nNome: " + (c1.getName()) + "\nTelefone: " + (c1.getTelefone()));
-            Console.WriteLine("CEP: " + (e1.getCep()) + "\nRua: " + e1.getRua() + "\nNúmero: " + e1.getNumero() + "\nBairro: " + e1.getBairro() + "\nCidade: " + e1.getCidade() + "\n");
+            Console.WriteLine("ID: " + c1.getId() + "\nNome: " + (c1.getName()) + "\nTelefone: " + MascaraTelefone(c1.getTelefone()));
+            Console.WriteLine("CEP: " + MascaraCEP(e1.getCep()) + "\nRua: " + e1.getRua() + "\nNúmero: " + e1.getNumero() + "\nBairro: " + e1.getBairro() + "\nCidade: " + e1.getCidade() + "\n");
 
             c1.setName("Alberto de Alcântara");
             c1.setTelefone("16998076556");
 
-            Console.WriteLine("Nome: " + (c1.getName()) + "\nTelefone: " + (c1.getTelefone()) + "\n");
+            Console.WriteLine("Nome: " + (c1.getName()) + "\nTelefone: " + MascaraTelefone(c1.getTelefone()) + "\n");
+        }
+        private static string MascaraTelefone(string strNumero)
+        {
+            string strMascara = "{0:(00)0000-0000}";
+            long lngNumero = Convert.ToInt64(strNumero);
+
+            if (strNumero.Length == 11)
+                strMascara = "{0:(00)00000-0000}";
+
+            return string.Format(strMascara, lngNumero);
+        }
+        private static string MascaraCEP(string strNumero1)
+        {
+            string strMascaraCep = "{0:00000-000}";
+            long lngNumero1 = Convert.ToInt64(strNumero1);
+
+            if (strNumero1.Length == 8)
+                strMascaraCep = "{0:00000-000}";
+
+            return string.Format(strMascaraCep, lngNumero1);
         }
     }   
 }
